1dcadd584a2ce1f1f077ba00591c15c073e3e0ec
Hackeem Mensah Bosu <66512968+Hackeemlunar@users.noreply.github.com>
2023-08-19 22:48:23 +0400

    added the project files and structures

---
+++ src/xyz/hacklunar/Main.java
@@ -0,0 +1,15 @@
+package xyz.hacklunar;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        StdDraw.setPenColor(Color.RED);
+        StdDraw.setCanvasSize(1000, 1000);
+        StdDraw.filledSquare(50,50, 10);
+        
+    }
+}
